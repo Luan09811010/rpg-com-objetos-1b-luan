da452e2e2dfd58791e8160066c724014a1a97b8f
Luan09811010 <admcleber946@gmail.com>
2026-09-22 12:09:16 -0300

    TERMINADO

--- app.js
+++ app.js
@@ -1,39 +1,102 @@
-const guerreiro = {
-    nome: "Thorin",
-    vida: 60,
-    ataque: 20,
+class Personagem {
+    constructor(nome, vida, ataque, defesa) {
+        this.nome = nome,
+        this.vida = vida,
+        this.vidaMax = vida,
+        this.ataque = ataque,
+        this.defesa = defesa
+    }
 
-    causaDano: function(personagem){
-    personagem.recebeDano(this.ataque)
+    curar(quantidade){
+        this.vida = this.vida + quantidade
 
-    recebeDano: function(quantidade){
-    this.vida = this.vida - quantidade
+        if(this.vida > this.vidaMax){
+            this.vida = this.vidaMax
+        }
+    }
+
+    causaDano(personagem) {
+        personagem.recebeDano(this.ataque)
+    }
+
+    recebeDano(quantidade) {
+        let dano = quantidade - this.defesa
+
+    if(quantidade < this.defesa) {
+        dano = 0
+    }
+
+    this.vida = this.vida - dano
+
+    if (this.vida <= 0) {
+            this.vida = "morreu"
+    }        
+    }
+
+    estaVivo(){
+        return this.vida > 0
+    }
+
+    mostraStatus(){
+        console.log(this.nome + " | vida: " + this.vida)
     }
 }
-    const mago = {
-    nome: "Gandalfe",
-    vida: 60,
-    ataque: 35,
 
-    recebeDano: function(quantidade){
-    this.vida = this.vida - quantidade
-    
-     causaDano: function(personagem){
-     personagem.recebeDano(this.ataque)
-
+class Guerreiro extends Personagem {
+    constructor(nome){
+        super(nome, 70, 35, 8)
+    }
+    golpePesado(personagem){
+        personagem.recebeDano(this.ataque * 2)
     }
 }
-const arqueiro = {
-nome: "Legolas",
-vida: 80,
-ataque: 25
 
-causaDano: function(personagem){
-personagem.recebeDano(this.ataque)
+const druida = new Personagem("Obin", 150, 15)
+const guerreiro = new Guerreiro("Thorin")
+const mago = new Personagem("Gandalfe", 60, 35, 10)
+const arqueiro = new Personagem("Legolas", 80, 25, 12)
 
-recebeDano: function(quantidade){
-this.vida = this.vida - quantidade
+const personagens = [
+    druida,
+    guerreiro,
+    mago,
+    arqueiro
+]
 
+personagens.forEach(function(personagem){
+personagem.mostraStatus()
+})
+
+let turno = 1
+let jogadorAtual = 0
+
+function proximoTurno(){
+    turno = turno + 1
+    jogadorAtual = jogadorAtual === 0 ? 1 : 0
 }
-guerreiro.causaDano(mago)
-console.log(mago.vida)
+
+const vidaGuerreiro = document.getElementById("vida-guerreiro")
+vidaGuerreiro.innerText = guerreiro.vida
+
+const ataqueGuerreiro = document.getElementById("ataque-guerreiro")
+ataqueGuerreiro.innerText = guerreiro.ataque
+
+const vidaMago = document.getElementById("vida-mago")
+vidaMago.innerText = mago.vida
+
+const guerreiroAtacar = document.getElementById("guerreiro-atacar")
+guerreiroAtacar.addEventListener("click", () => {
+    guerreiro.causaDano(mago)
+    vidaMago.innerText = mago.vida
+})
+
+
+
+const ataqueMago = document.getElementById("ataque-mago")
+ataqueMago.innerText = mago.ataque
+
+const magoAtacar = document.getElementById("mago-atacar")
+magoAtacar.addEventListener("click", () => {
+    mago.causaDano(guerreiro)
+    vidaGuerreiro.innerText = guerreiro.vida
+})
